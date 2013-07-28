69f2ba283b13d28cc967660ed9cfd523f92c5daa
4nt01ne <antoine.wils@gmail.com>
2013-07-28 22:43:54 +0200

    camel quick off: some camel tests

---
+++ src/test/java/be/ewils/decentralize/account/CreateAccountTest.java
@@ -0,0 +1,37 @@
+package be.ewils.decentralize.account;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class CreateAccountTest extends CamelTestSupport {
+
+    @EndpointInject(uri = "mock:findAccounts")
+    protected MockEndpoint resultEndpoint;
+    @Produce(uri = "direct:listAccounts")
+    protected ProducerTemplate template;
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:listAccounts").to("mock:findAccounts");
+            }
+        };
+    }
+
+    @Test
+    public void testFromListAccountsToFindAccounts() throws Exception {
+        String expectedBody = "<listAccounts><id>123</id></listAccounts>";
+
+        resultEndpoint.expectedBodiesReceived(expectedBody);
+
+        template.sendBody(expectedBody);
+
+        resultEndpoint.assertIsSatisfied();
+    }
+}
